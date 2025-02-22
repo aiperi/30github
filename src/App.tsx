9a187cf6824d53e247b26eadbcca0c9d53173094
aiperi_mac <aiperi.akunova@gmail.com>
2025-02-22 17:35:43 +0600

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ function App() {
                 </p>
                   <Card/>
                  <GradientCard/>
-              <AnimatedDivs/>
               </div>
             </div>
           </div>
