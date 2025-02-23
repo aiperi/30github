8f612ec45c3c7f7cab664f7026b9ff959c8866d8
aiperi_mac <aiperi.akunova@gmail.com>
2025-02-24 00:58:39 +0600

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,7 @@ function App() {
                 <p className="card-text">
                   This is a challenge to get 30 stars on github
                 </p>
-                  <p className="card-text">
-                      This is a challenge to get 30 stars on github
-                  </p>
+
                   <Card/>
                  <GradientCard/>
               </div>
